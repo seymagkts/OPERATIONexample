7b6ad731ae7044494ca19166bf1d768ae7f0be39
Seyma <63148309+lacilobaid@users.noreply.github.com>
2021-07-22 23:31:59 +0300

    Create yönetici.java

---
+++ yönetici.java
@@ -0,0 +1,23 @@
+public class yönetici extends calisanBase{
+    private int sorumluKisi;
+
+    public yönetici(String adSoyad,int id,int sorumluKisi) {
+        super(adSoyad, id);
+        this.setSorumluKisi(sorumluKisi);
+    }
+    public void bilgiGoster(){
+        super.bilgiGoster();
+        System.out.println("Sorumlu oldugu kisi sayisi: "+sorumluKisi );
+    }
+    public void zamYap(int zam){
+        System.out.println(zam+"tl yapildi...");
+    }
+
+    public int getSorumluKisi() {
+        return sorumluKisi;
+    }
+
+    public void setSorumluKisi(int sorumluKisi) {
+        this.sorumluKisi = sorumluKisi;
+    }
+}
